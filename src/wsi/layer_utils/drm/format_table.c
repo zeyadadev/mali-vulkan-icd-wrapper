e68db9a7f17706a6e04cfbbfc3b7a248a861b821
Ayman Zeyada <zeyada.ayman@gmail.com>
2026-07-03 16:40:51 +0300

    wsi/drm: stop advertising 10-bit dmabuf formats

--- src/wsi/layer_utils/drm/format_table.c
+++ src/wsi/layer_utils/drm/format_table.c
@@ -57,8 +57,7 @@ const fmt_spec fourcc_format_table[] = {
    { DRM_FORMAT_RGBA8888, 1, { 32, 0, 0, 0 }, VK_FORMAT_UNDEFINED },
    { DRM_FORMAT_BGRA8888, 1, { 32, 0, 0, 0 }, VK_FORMAT_UNDEFINED },
 
-   { DRM_FORMAT_ARGB2101010, 1, { 32, 0, 0, 0 }, VK_FORMAT_A2R10G10B10_UNORM_PACK32 },
-   { DRM_FORMAT_ABGR2101010, 1, { 32, 0, 0, 0 }, VK_FORMAT_A2B10G10R10_UNORM_PACK32 },
+   //{ DRM_FORMAT_ABGR2101010, 1, { 32, 0, 0, 0 }, VK_FORMAT_A2B10G10R10_UNORM_PACK32 },
 };
 
 const fmt_spec srgb_fourcc_format_table[] = {
